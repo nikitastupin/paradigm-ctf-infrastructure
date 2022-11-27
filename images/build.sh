b9c005a925b7f0f13792757df74edd1951dc5542
Nikita Stupin <18281368+nikitastupin@users.noreply.github.com>
2022-11-27 13:40:04 +0100

    Detach base and eth-base from gcr.io/paradigmxyz

--- images/build.sh
+++ images/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-(cd challenge-base && docker buildx build --push --platform linux/amd64 . -t gcr.io/paradigmxyz/ctf/base:latest)
-(cd eth-challenge-base && docker buildx build --push --platform linux/amd64 . -t gcr.io/paradigmxyz/ctf/eth-base:latest)
-(cd cairo-challenge-base && docker buildx build --push --platform linux/amd64 . -t gcr.io/paradigmxyz/ctf/cairo-base:latest)
+DOCKERHUB_USERNAME="${DOCKERHUB_USERNAME:-nikitastupin}"
+
+(cd challenge-base && docker build -t "$DOCKERHUB_USERNAME/ctf/base:latest" .)
+(cd eth-challenge-base && docker build -t "$DOCKERHUB_USERNAME/ctf/eth-base:latest" --build-arg "DOCKERHUB_USERNAME=$DOCKERHUB_USERNAME" .)
+# (cd cairo-challenge-base && docker buildx build --push --platform linux/amd64 . -t gcr.io/paradigmxyz/ctf/cairo-base:latest)
